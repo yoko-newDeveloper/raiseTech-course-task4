4d6a9119a26464c00c662a8b4e6bcab6a548298c
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-04 17:12:06 +0900

    拡張for文

--- src/main/java/Stream/sweetsBox/ExpansionSweetsBox.java
+++ src/main/java/Stream/sweetsBox/ExpansionSweetsBox.java
@@ -1,4 +1,4 @@
-package Stream;
+package Stream.sweetsBox;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,15 +6,12 @@ import java.util.List;
 public class ExpansionSweetsBox {
     public static void main(String[] args) {
 
-        List<String> sweets = new ArrayList<String>();
-        sweets.add("クッキー");
-        sweets.add("チョコレート");
-        sweets.add("せんべい");
-        sweets.add("ガム");
+        // List.of使用
+        List<String> sweets = List.of("cookie", "chocolate", "candy", "gum","chocolate");
 
         // ラムダ式を拡張for文で記述する
-        for (String box : sweets) {
-            System.out.println(box);
+        for (String sweet : sweets) {
+            System.out.println(sweets);
         }
 
 
