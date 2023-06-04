5299d8ee7088c69c4679f3129200c05bd0b29d38
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-04 17:18:00 +0900

    forEachとラムダ式

---
+++ src/main/java/Stream/sweetsBox/ForEachSweetsBox.java
@@ -0,0 +1,20 @@
+package Stream.sweetsBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForEachSweetsBox {
+    public static void main(String[] args) {
+
+        List<String> sweets = new ArrayList<String>();
+
+        sweets.add("cookie");
+        sweets.add("chocolate");
+        sweets.add("candy");
+        sweets.add("gum");
+        sweets.add("chocolate");
+
+        // ラムダ式
+        sweets.forEach(sweet -> System.out.println(sweets));
+    }
+}
