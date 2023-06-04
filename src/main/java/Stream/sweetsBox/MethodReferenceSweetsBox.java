49ad83ebcca2f48b9397cb81597ab57bec9a8e15
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-04 17:26:04 +0900

    メソッド参照

--- src/main/java/Stream/sweetsBox/MethodReferenceSweetsBox.java
+++ src/main/java/Stream/sweetsBox/MethodReferenceSweetsBox.java
@@ -1,2 +1,16 @@
-package Stream.sweetsBox;public class MethosReferenceSweetsBox {
+package Stream.sweetsBox;
+
+import java.util.List;
+
+public class MethodReferenceSweetsBox {
+    public static void main(String[] args) {
+        List<String> sweets = List.of("cookie", "chocolate", "candy", "gum", "chocolate");
+        long chocolateCount = sweets.stream()
+
+                // ラムダ式をメソッド参照に変更する
+                .filter("chocolate" :: contains)
+                .count();
+
+        System.out.println("chocolateの数：" + chocolateCount);
+    }
 }
