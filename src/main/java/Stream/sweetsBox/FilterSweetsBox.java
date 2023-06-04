228e17c0c9358b84aa6098f9ddbd34d39cdc6714
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-04 17:23:30 +0900

    フィルタリング

--- src/main/java/Stream/sweetsBox/FilterSweetsBox.java
+++ src/main/java/Stream/sweetsBox/FilterSweetsBox.java
@@ -1,2 +1,13 @@
-package Stream.sweetsBox;public class FilterSweetBox {
+package Stream.sweetsBox;
+
+import java.util.List;
+
+public class FilterSweetsBox {
+    public static void main(String[] args) {
+        List<String> sweets = List.of ("cookie", "chocolate", "candy", "gum", "chocolate");
+        List<String> chocolateCount = sweets.stream()         // Listをstreamに変換する
+                .filter(sweet -> sweet.contains("chocolate")) // chocolateをフィルター
+                .toList();                                    // chocolateの数を数える
+        System.out.println(chocolateCount);
+    }
 }
