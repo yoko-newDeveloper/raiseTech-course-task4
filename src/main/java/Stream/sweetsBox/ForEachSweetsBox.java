caebe118ec18548db2cc4974c58e207e9a638a41
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-04 17:59:35 +0900

    ArrayList<String>をArrayList<>に修正

--- src/main/java/Stream/sweetsBox/ForEachSweetsBox.java
+++ src/main/java/Stream/sweetsBox/ForEachSweetsBox.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ForEachSweetsBox {
     public static void main(String[] args) {
 
-        List<String> sweets = new ArrayList<String>();
+        List<String> sweets = new ArrayList<>();
 
         sweets.add("cookie");
         sweets.add("chocolate");
