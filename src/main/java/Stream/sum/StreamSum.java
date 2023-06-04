148ff21aa0f859c9cfce3db34523061b444ce1bc
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-04 20:02:46 +0900

    ArrayList<String>をArrayList<>に修正

--- src/main/java/Stream/sum/StreamSum.java
+++ src/main/java/Stream/sum/StreamSum.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class StreamSum {
     public static void main(String[] args) {
 
-        List<String> list = new ArrayList<String>();
+        List<String> list = new ArrayList<>();
 
         list.add("1");
         list.add("2");
@@ -16,7 +16,7 @@ public class StreamSum {
 
         // Stream
         int sum = list.stream()   // listをStreamに変換する
-                .mapToInt(i -> Integer.parseInt(i)) // StreamをIntStreamに変換する
+                .mapToInt(i -> Integer.parseInt(i))  // StreamをIntStreamに変換する(敢えてメソッド参照に変更しない）
                 .sum();           // intに変換された要素を合計する
 
         System.out.println(sum);
