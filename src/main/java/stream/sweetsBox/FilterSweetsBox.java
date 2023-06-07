d7cc4cf73e7d0ef3d59f852624b4f4439e2ba4f0
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-06-08 06:40:25 +0900

    L7:List.ofの不要なスペースを削除

--- src/main/java/stream/sweetsBox/FilterSweetsBox.java
+++ src/main/java/stream/sweetsBox/FilterSweetsBox.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class FilterSweetsBox {
     public static void main(String[] args) {
-        List<String> sweets = List.of ("cookie", "chocolate", "candy", "gum", "chocolate");
+        List<String> sweets = List.of("cookie", "chocolate", "candy", "gum", "chocolate");
         List<String> chocolateCount = sweets.stream()         // Listをstreamに変換する
                 .filter(sweet -> sweet.contains("chocolate")) // chocolateをフィルター
                 .toList();                                    // chocolateの数を数える
